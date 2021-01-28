a9e45c4e50215373b15aa1b90fd05fc9ec487ab3
secrettttt <243950548@qq.com>
2021-01-28 23:46:41 +0800

    fix: 去掉多余的import

--- src/main/java/com/skytower/service/impl/UserServiceImpl.java
+++ src/main/java/com/skytower/service/impl/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.skytower.service.impl;
 import com.skytower.dao.UserMapper;
 import com.skytower.entry.UserEntry;
 import com.skytower.service.UserService;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
